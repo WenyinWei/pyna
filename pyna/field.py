ca4e26e9d2d7e2a04d2ec59ba19ef81b6d7ad147
WenyinWei <wenyin.wei.ww@gmail.com>
2022-07-26 04:08:31 +0800

    + RegularCylindricalGridField

--- pyna/field.py
+++ pyna/field.py
@@ -1,4 +1,4 @@
-class GridField:
+class RegualrCylindricalGridField:
     def __init__(self, R, Z, Phi, BR, BZ, BPhi) -> None:
         self._R = R
         self._Z = Z
@@ -6,3 +6,22 @@ class GridField:
         self._BR = BR
         self._BZ = BZ
         self._BPhi = BPhi
+
+    @property
+    def R(self):
+        return self._R
+    @property
+    def Z(self):
+        return self._Z
+    @property
+    def Phi(self):
+        return self._Phi
+    @property
+    def BR(self):
+        return self._BR
+    @property
+    def BZ(self):
+        return self._BZ
+    @property
+    def BPhi(self):
+        return self._BPhi
