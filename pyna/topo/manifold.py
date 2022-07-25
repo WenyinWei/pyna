53e05a80fc8fff3663169acea51c30d64a5d715c
WenyinWei <wenyin.wei.ww@gmail.com>
2022-07-26 03:56:16 +0800

    + a function: grow_manifold_from_Xcycle_naive_carousel

--- pyna/topo/manifold.py
+++ pyna/topo/manifold.py
@@ -1,9 +1,65 @@
+from multiprocessing.sharedctypes import Value
 from pyna.diff.fieldline import _FieldDifferenatiableRZ
-from pyna.diff.fixedpoint import Jac_evolution_along_Xcycle, eigvec_interpolator_along_Xcycle
+from pyna.diff.cycle import eigvec_interpolator_along_Xcycle
 from scipy.integrate import solve_ivp
 import numpy as np
+from pyna.flt import bundle_tracing_with_t_as_DeltaPhi
+def grow_manifold_from_Xcycle_naive_carousel(R, Z, Phi, BR, BZ, BPhi, Xcycle_RZdiff, Jac_evosol_along_Xcycle, eigind:int, Phi_span, W_nPhi:int, Ind_num:int, first_step=5e-5, max_step=1e-4):    
+    Phi_start, Phi_end = Phi_span[0], Phi_span[1]
+    W_Phi = np.linspace(Phi_start, Phi_end, num=W_nPhi, endpoint=True)
+    W_dPhi = W_Phi[1]-W_Phi[0]
+#     fltres_list = []
+#     while True:
+#         try:
+#             fltres_list.append( 
+#                 bundle_tracing_with_t_as_DeltaPhi(R,Z,Phi,BR,BZ,BPhi, 5.0, testarr) 
+#             ) # fltres.sol.mat_interp(2.0)
+#         except:
+#             break
 
+    Xcycle_RZ_arr = Xcycle_RZdiff[0].sol(W_Phi).T # in shape [W_nPhi, 2]
 
+    # Use eigind : 1, 2, 3, 4. 1 & 3 are contrary, 2 & 4 are contrary.
+    if eigind in [1, 2]:
+        eigind_equiv = eigind 
+    elif eigind in [3, 4]: 
+        eigind_equiv = eigind - 2
+    else:
+        raise ValueError("eigind should be either 1, 2, 3, or 4 integer.")
+    eigval_interp, eigvec_interp = eigvec_interpolator_along_Xcycle(Jac_evosol_along_Xcycle)
+
+    Xcycle_eigvec = eigvec_interp(W_Phi)[:,:,eigind_equiv]
+    W_PhiInd_RZ = np.empty( (W_nPhi, Ind_num, 2) ) 
+    if eigind in [1, 2]:
+        Xcycle_bitshift_along_eigvec_RZ = Xcycle_RZ_arr + first_step * Xcycle_eigvec
+    elif eigind in [3, 4]: 
+        Xcycle_bitshift_along_eigvec_RZ = Xcycle_RZ_arr - first_step * Xcycle_eigvec
+    W_PhiInd_RZ[:,1,:] = Xcycle_bitshift_along_eigvec_RZ
+    W_PhiInd_s = np.empty( (W_nPhi, Ind_num,) )
+    W_PhiInd_s[:,0], W_PhiInd_s[:,1] = 0.0, first_step # initial cycle s = 0, the bitshift cycle in the direction of eigenvector s = first_step
+    
+    total_DeltaPhi = Ind_num * W_dPhi
+    initpts_RZPhi = np.stack( (W_PhiInd_RZ[:,1,0], W_PhiInd_RZ[:,1,1], W_Phi) , axis=1)[:-1,:] # in shape of [W_nPhi-1, 3]
+
+    if eigval_interp(Phi_start)[eigind_equiv] > 1.0: # Unstable manifold of Phi-increasing Poincare map
+        fltres = bundle_tracing_with_t_as_DeltaPhi(R, Z, Phi, BR, BZ, BPhi, total_DeltaPhi, initpts_RZPhi, pos_or_neg=True,)
+        for i in range(2, Ind_num):
+            W_PhiInd_RZ[:-1,i,:] = np.roll( fltres.sol.mat_interp( (i-1)*W_dPhi )[:,:-1], i-1, axis=0 ) # in shape of [W_nPhi-1, 2]
+    elif 0.0 < eigval_interp(Phi_start)[eigind_equiv] < 1.0: # Stable manifold of Phi-increasing Poincare map
+        fltres = bundle_tracing_with_t_as_DeltaPhi(R, Z, Phi, BR, BZ, BPhi, total_DeltaPhi, initpts_RZPhi, pos_or_neg=False,)
+        for i in range(2, Ind_num):
+            W_PhiInd_RZ[:-1,i,:] = np.roll( fltres.sol.mat_interp( (i-1)*W_dPhi )[:,:-1], -(i-1), axis=0 ) # in shape of [W_nPhi-1, 2]
+    else:
+        raise NotImplementedError("We have not yet implemented the grow_manifold_from_Xcycle function for Mobiusian cycles.")
+    
+    W_PhiInd_RZ[-1,:,:] = W_PhiInd_RZ[0,:,:] # seam the head and tail of manifold
+    
+    for i in range(2, Ind_num):
+        W_PhiInd_s[:,i] = W_PhiInd_s[:,i-1] + np.sqrt(
+            (W_PhiInd_RZ[:,i,0] - W_PhiInd_RZ[:,i-1,0])**2 
+          + (W_PhiInd_RZ[:,i,1] - W_PhiInd_RZ[:,i-1,1])**2)
+    
+    return W_Phi, W_PhiInd_RZ, W_PhiInd_s
 
 def _central_finite_difference_first_derivative(arr:np.ndarray, dPhi:float, accuracy_order=4):
     """1st derivative of a periodic function with spacing dPhi
@@ -33,7 +89,9 @@ def _central_finite_difference_first_derivative(arr:np.ndarray, dPhi:float, accu
             + (np.roll(arr, 2) - np.roll(arr, -2) )  * ( -1 / 5 ) \
             + (np.roll(arr, 3) - np.roll(arr, -3) )  * ( 4 / 105 ) \
             + (np.roll(arr, 4) - np.roll(arr, -4) )  * ( -1 / 280 )  ) / dPhi
-        
+
+from deprecated import deprecated
+@deprecated(version='0.0.2', reason="Numerically unstable algorithm, try grow_manifold_from_Xcycle instead.")
 def grow_manifold_from_Xcycle_eig_interp(R, Z, Phi, BR, BZ, BPhi, Xcycle_RZdiff, Jac_evosol_along_Xcycle, eigind, S_span, S_num:int, Phi_span, Phi_num:int, rev_eigvec=False, first_step=5e-5, max_step=1e-4):
     
     Phi_start, Phi_end = Phi_span[0], Phi_span[1]
@@ -94,11 +152,11 @@ def grow_manifold_from_Xcycle_eig_interp(R, Z, Phi, BR, BZ, BPhi, Xcycle_RZdiff,
 
     if StaOrNot:
         manifold_sol = solve_ivp(Sta_manifold_growth_ODE, S_span, 
-                        RZ_Xcycle_bitshift_along_eigvec.reshape( (2*(nPhi_manifold-1),), order='F' ), 
+                        RZ_Xcycle_bitshift_along_eigvec.reshape( (2*(nPhi_manifold-1),), order='F' ), method="LSODA", 
                         dense_output=True, first_step=first_step, max_step=max_step)
     else:
         manifold_sol = solve_ivp(Usta_manifold_growth_ODE, S_span, 
-                        RZ_Xcycle_bitshift_along_eigvec.reshape( (2*(nPhi_manifold-1),), order='F' ), 
+                        RZ_Xcycle_bitshift_along_eigvec.reshape( (2*(nPhi_manifold-1),), order='F' ), method="LSODA",
                         dense_output=True, first_step=first_step, max_step=max_step)
 
     S_arr = np.empty( (S_num) )
@@ -171,7 +229,7 @@ def smooth1D(x, window_len=11, window='hanning'):
     y=np.convolve(w/w.sum(),s,mode='valid')
     return y
 
-def grow_manifold_from_Xcycle(R, Z, Phi, BR, BZ, BPhi, Xcycle_RZdiff, Xcycle_eig_theta, S_span, S_num:int, Phi_span, Phi_num:int, rev_eigvec=False, first_step=5e-5, max_step=1e-4):
+def grow_manifold_from_Xcycle_by_formula(R, Z, Phi, BR, BZ, BPhi, Xcycle_RZdiff, Xcycle_eig_theta, S_span, S_num:int, Phi_span, Phi_num:int, rev_eigvec=False, first_step=5e-5, max_step=1e-4):
 
     Phi_start, Phi_end = Phi_span[0], Phi_span[1]
     dPhi = Phi[1]-Phi[0]
@@ -231,11 +289,11 @@ def grow_manifold_from_Xcycle(R, Z, Phi, BR, BZ, BPhi, Xcycle_RZdiff, Xcycle_eig
 
     if StaOrNot:
         manifold_sol = solve_ivp(Sta_manifold_growth_ODE, S_span, 
-                        RZ_Xcycle_bitshift_along_eigvec.reshape( (2*(nPhi_manifold-1),), order='F' ), 
+                        RZ_Xcycle_bitshift_along_eigvec.reshape( (2*(nPhi_manifold-1),), order='F' ), method="LSODA",
                         dense_output=True, first_step=first_step, max_step=max_step)
     else:
         manifold_sol = solve_ivp(Usta_manifold_growth_ODE, S_span, 
-                        RZ_Xcycle_bitshift_along_eigvec.reshape( (2*(nPhi_manifold-1),), order='F' ), 
+                        RZ_Xcycle_bitshift_along_eigvec.reshape( (2*(nPhi_manifold-1),), order='F' ), method="LSODA",
                         dense_output=True, first_step=first_step, max_step=max_step)
 
     S_arr = np.empty( (S_num) )
