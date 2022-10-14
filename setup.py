ce21ba37457efc9b35d9c274008713fc38bd2104
Wenyin Wei <wenyin.wei.ww@gmail.com>
2022-10-14 10:09:16 +0800

    + install_requries, ver -> 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyna-chaos",
-    version="0.0.1",
+    version="0.0.2",
     author="Wenyin Wei",
     author_email="wenyin.wei.ww@gmail.com",
     description="Python package for DYNAmics (dynamical systems)",
@@ -20,4 +20,10 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',
+    install_requires=[
+        "scipy", "sympy", "numpy", "pandas", # fundamental python packages
+        "matplotlib", "plotly",
+        "jupyter", "notebook", "ipympl", # "ipympl" for %matplotlib widget
+        "sparse", # for storing data of high order derivatives inter-relation
+    ]
 )
