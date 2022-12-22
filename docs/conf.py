9dd52b496d87c8e2dabad378807640a9f18f11c8
Wenyin Wei <wenyin.wei.ww@gmail.com>
2022-12-22 22:56:47 +0800

    apidoc -> autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('../'))
 
 project = 'pyna'
 copyright = '2022, Wenyin Wei'
@@ -18,7 +18,7 @@ author = 'Wenyin Wei'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinxcontrib.apidoc',
+#     'sphinxcontrib.apidoc',
     'sphinx.ext.autodoc', 
     'sphinx.ext.doctest', 
     'sphinx.ext.todo', 
@@ -28,10 +28,22 @@ extensions = [
     'sphinx.ext.githubpages', 
     'sphinx.ext.napoleon', # to let sphinx understand Google/Numpy style comment
     ]
-apidoc_module_dir = '../pyna'
-apidoc_output_dir = './docs/api'
-apidoc_excluded_paths = [ ]
-apidoc_separate_modules = True
+# apidoc_module_dir = '../pyna'
+# apidoc_output_dir = './docs/api'
+# apidoc_excluded_paths = [ ]
+# apidoc_separate_modules = True
+
+autosummary_generate = True
+
+autodoc_default_options = {
+    'members': True,
+    # The ones below should be optional but work nicely together with
+    # example_package/autodoctest/doc/source/_templates/autosummary/class.rst
+    # and other defaults in sphinx-autodoc.
+    'show-inheritance': True,
+    'inherited-members': True,
+    'no-special-members': True,
+}
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
