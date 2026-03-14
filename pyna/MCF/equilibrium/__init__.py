24510690b2f6f7d5e3aeffea73cc964e7b4a74ce
WenyinWei <wenyin.wei.ww@gmail.com>
2026-03-14 11:15:44 +0800

    feat: scaffold 3D beta-feedback in Boozer coordinates (lower priority, parallel to cylindrical)

--- pyna/MCF/equilibrium/__init__.py
+++ pyna/MCF/equilibrium/__init__.py
@@ -3,6 +3,11 @@ from pyna.MCF.equilibrium.axisymmetric import AxisymEquilibrium, SyntheticCircul
 from pyna.MCF.equilibrium.Solovev import SolovevEquilibrium
 from pyna.MCF.equilibrium.GradShafranov import recover_pressure_simplest, solve_GS_perturbed
 from pyna.MCF.equilibrium.stellarator import SimpleStellarartor, simple_stellarator
+from pyna.MCF.equilibrium.feedback_boozer import (
+    BoozerSurface,
+    BoozerPerturbation,
+    compute_boozer_response,
+)
 from pyna.MCF.equilibrium.feedback_cylindrical import (
     CylindricalGrid,
     PerturbationField,
