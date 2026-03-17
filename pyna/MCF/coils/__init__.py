7733126a7361edc07aee14d79c2808ccd90e14c0
Wenyin Wei <wenyin@mail.ustc.edu.cn>
2026-03-17 17:58:28 +0800

    refactor(coils): remove bluestar imports from __init__
    bluestar-specific symbols (CoilGeometry, load_coil_directory,
    compute_vacuum_field, bluestar_vacuum_field, analytic_circular_coil_field)
    moved to private topoquest repo.

--- pyna/MCF/coils/__init__.py
+++ pyna/MCF/coils/__init__.py
@@ -5,11 +5,3 @@ from pyna.MCF.coils.coil_system import CoilSet, Biot_Savart_field, biot_savart_f
 from pyna.MCF.coils.field import CylindricalGridVectorField3D, CylindricalGridAxiVectorField3D
 from pyna.MCF.coils.RMP import normalize_b, rmp_spectrum_2d, island_width_at_rational_surfaces
 from pyna.MCF.coils.vector_potential import CoilFieldVectorPotential
-from pyna.MCF.coils.bluestar import (
-    CoilGeometry,
-    load_coil_directory,
-    compute_vacuum_field,
-    bluestar_vacuum_field,
-    analytic_circular_coil_field,
-    biot_savart_field,
-)
