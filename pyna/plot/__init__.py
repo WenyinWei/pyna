fb7250540b92d7ab9aef3cb0f06bb2e14673b3e6
Wenyin Wei <wenyin@YuanchuWenyin.localdomain>
2026-05-25 17:06:15 +0800

    Remove stream-only poloidal plotting helper

--- pyna/plot/__init__.py
+++ pyna/plot/__init__.py
@@ -4,13 +4,6 @@ from pyna.plot.island import plot_island, island_section_points
 from pyna.plot.island_chain import plot_island_chain, island_chain_section_points
 from pyna.plot.xo_points import draw_xo_points, XO_STYLE
 from pyna.plot.poincare_beta import plot_poincare_beta_grid
-from pyna.plot.poloidal import (
-    B_pol_section,
-    poloidal_field_section,
-    robust_field_max,
-    s_isolines,
-    stream_field_from_components,
-)
 from pyna.plot.tube import (
     plot_tube_section,
     plot_tube_chain_section,
@@ -30,11 +23,6 @@ __all__ = [
     "draw_xo_points",
     "XO_STYLE",
     "plot_poincare_beta_grid",
-    "poloidal_field_section",
-    "B_pol_section",
-    "stream_field_from_components",
-    "robust_field_max",
-    "s_isolines",
     # Tube / TubeChain (first-class)
     "plot_tube_section",
     "plot_tube_chain_section",
