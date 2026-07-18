96d4c6d9213efb4ad6764e1ddf74a3b8066f4557
Wenyin Wei <wenyin.wei.ww@gmail.com>
2026-07-18 17:37:27 +0800

    chore(release): bump version to 0.9.6

--- pyna/__init__.py
+++ pyna/__init__.py
@@ -11,7 +11,7 @@ toroidal  : Toroidal geometry and field-line utilities
     ``pyna.coord`` has been removed. ``pyna.mag`` remains as a
     compatibility facade; prefer ``pyna.toroidal.control`` in new code.
 """
-__version__ = "0.9.5"
+__version__ = "0.9.6"
 
 from importlib import import_module
 
